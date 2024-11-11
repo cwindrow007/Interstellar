686ec84394dac9cee014821ad95da3425a7b6056
Chris <ChristoWindow@gmail.com>
2024-11-10 19:53:05 -0500

    First Commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const {Client, GatewayIntentBits} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.id} is online`);
+});
+
+client.login(process.env.TOKEN);
