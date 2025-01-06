bf9adb44d83a8b2762d01c3f4dc6ae5eca31af1f
Chris <ChristoWindow@gmail.com>
2025-01-05 21:59:52 -0500

    JavaScript to Python Conversion

---
+++ Bot Code/Main.py
@@ -0,0 +1,30 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+#loader for environment vairables
+
+load_dotenv()
+BOT_TOKEN = os.getenv('TOKEN')
+
+#Bot setup
+intents = discord.Intents.default()
+intents.messages = True #Message Tracking
+intents.message_content = True #Reading Message content
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+#Event: Bot is ready
+@bot.event
+async def on_ready():
+    print('Logged in as {bot.user} (ID: {bot.user.id})')
+
+#Error handling
+
+#Load Extensions
+
+#loader
+if __name__ == '__main__':
+    bot.run(BOT_TOKEN)
+
